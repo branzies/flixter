3eb13930415854e9a81de3489db647d225f38e2b
Brandon <tobrandonho@gmail.com>
2019-11-19 02:31:02 +0000

    push

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+before_action :authenticate_user! #only authenticated users can view lessons page
+before_action :require_user_enrolled_in_course, only: [:show] #ensure current user is enrolled in the course
+
   def show
   end
 
@@ -7,5 +10,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_user_enrolled_in_course
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled to view that lesson.'
+    end
   end
 end
